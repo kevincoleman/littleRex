f843904a0a47a299f3b4d0e04724eff326f1ec42
kevincoleman <kevin.n.coleman@gmail.com>
2015-05-15 15:58:03 -0700

    back to port 5000

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/', index);
 
 module.exports = app;
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });

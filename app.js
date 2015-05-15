a39e1bbc1a8fbe5337c29a0f128384e6a2b1ee69
kevincoleman <kevin.n.coleman@gmail.com>
2015-05-15 15:55:39 -0700

    updated to port 80

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/', index);
 
 module.exports = app;
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
